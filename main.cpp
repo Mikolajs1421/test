1293433cf7db58417505a93ef3ca9f6991b89b0d
Mikolaj Szlaski <12497@student.pansim.edu.pl>
2024-06-13 12:40:27 +0200

    list

--- main.cpp
+++ main.cpp
@@ -1,9 +1,92 @@
 #include <iostream>
 
 using namespace std;
+class Node {
+    public:
+    int value;
+    Node* next;
 
-int main(){
-    cout << "Hello World";
+    Node() {
+        value =0;
+        next = nullptr
+    }
+
+    Node(int val){
+        value = val;
+        next = nullptr
+    }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+
+    public:
+        LinkedList() {
+            head = nullptr;
+        }
+
+        void add(Node* n){
+            if (head == nullptr){
+                head = n;
+            }else{
+                Node* temp = head;
+                while (temp->next != nullptr){
+                    temp = temp->next;
+                }
+                temp->next = n;
+            }
+        }
+
+        Node* get(int index) {
+            if (index <0) return nullptr;
+            Node* temp = head;
+            int count = 0;
+            while (temp != nullptr){
+                if (count == index){
+                    return temp;
+                }
+                count++;
+                temp = temp->next;
+            }
+            return nullptr;
+        }
+
+        Node* search(int value){
+            Node* temp = head;
+            while (temp != nullptr) {
+                if (temp != nullptr) {
+                    return temp;
+                }
+                temp = temp->next;
+            }
+            return nullptr;
+        }
+};
+
+int main() {
+    LinkedList lista;
+
+    list.add(new Node(1));
+    list.add(new Node(2));
+    list.add(new Node(3));
+
+
+    Node* result = nullptr;
+    for (Node* temp = list.head; temp != nullptr; temp = temp->next) {
+        if (temp->value == 2) {
+            result = temp;
+            break;
+        }
+    }
+
+
+    if (result != nullptr) {
+        cout << "Znaleziono wzel z wartoscia 2: " << result->value << endl;
+    }else{
+        cout << "Wezel z wartoscia 2 nie zostalznaleziony" << endl;
+    }
 
     return 0;
+
 }
